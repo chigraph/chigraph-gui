fb39677bb93b7999c3b26e1a104ebb6c1bc1ab03
Aun-Ali Zaidi <admin@kodeit.net>
2017-04-14 01:17:48 -0500

    ci: Add Travis CI infrastructure
    Import the Travis CI files from the chigraph/ repo and modify them
    for use by the GUI.
    
    BUG=none
    TEST=none [CI Build Dependant]
    
    Signed-off-by: Aun-Ali Zaidi <admin@kodeit.net>

---
+++ .travis/build.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -xe
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+
+	if [ -n "$TEST_COV" ]; then
+
+		cmake . -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+			-DCMAKE_CXX_COMPILER=$CXX_COMPILER \
+			-DCMAKE_C_COMPILER=$C_COMPILER \
+			-GNinja -DCMAKE_CXX_FLAGS='--coverage' \
+			-DLLVM_CONFIG="/usr/lib/llvm-${LLVM_VERSION}/bin/llvm-config"
+	else
+
+		cmake . -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+			-DCMAKE_CXX_COMPILER=$CXX_COMPILER \
+			-DCMAKE_C_COMPILER=$C_COMPILER \
+			-GNinja \
+			-DLLVM_CONFIG="/usr/lib/llvm-${LLVM_VERSION}/bin/llvm-config"
+
+	fi
+
+	ninja
+	CTEST_OUTPUT_ON_FAILURE=1 ninja test
+
+	if [ -n "$TEST_COV" ]; then
+		pip install --user cpp-coveralls
+		coveralls --exclude /usr --exclude third_party --exclude chigraphgui --exclude test --exclude build --include libchigraph --include libchigraphdebugger --include chi --exclude libchigraph/include/chi/json.hpp
+	fi
+
+else if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+
+	cmake . \
+		-DCMAKE_PREFIX_PATH='/usr/local/opt/qt5/;/usr/local/opt/gettext' \
+		-DCMAKE_BUILD_TYPE=Debug \
+		-DLLVM_CONFIG=/usr/local/opt/llvm/bin/llvm-config \
+		-GNinja -DCG_BUILD_DEBUGGER=OFF
+	ninja
+	# ninja test
+
+fi
---
+++ .travis/install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -xe
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+
+	if [ "$LLVM_VERSION" == "3.9" ] || [ "$LLVM_VERSION" == "4.0" ]; then
+		sudo apt-get install liblldb-${LLVM_VERSION}-dev
+	else
+		sudo apt-get install lldb-${LLVM_VERSION}-dev
+	fi
+
+	bash ./setup.sh
+
+else if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+
+	brew install cmake qt5 bison gettext ninja python3 || echo
+	brew install llvm --with-lldb
+
+	brew tap chigraph/kf5
+	brew install kf5-extra-cmake-module
+	brew install kf5-karchive	kf5-ktexteditor		kf5-kjobwidgets	 \
+		     kf5-kdbusaddons	kf5-kio			kf5-kcrash	 \
+		     kf5-sonnet		kf5-syntax-highlighting	kf5-kparts	 \
+		     kf5-kguiaddons	kf5-kitemviews		kf5-kconfig	 \
+		     kf5-kconfigwidgets	kf5-kauth		kf5-kcodecs	 \
+		     kf5-kcompletion	kf5-kglobalaccel	kf5-kservice	 \
+		     kf5-kwindowsystem	kf5-kcoreaddons		kf5-ki18n	 \
+		     kf5-kxmlgui	kf5-kwidgetsaddons	kf5-ktextwidgets \
+		     kf5-kiconthemes
+
+	bash ./setup.sh
+
+fi
