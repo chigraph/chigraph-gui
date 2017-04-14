3fffdf8d00df0e3f6a0dcb925edf7625d8862e82
Russell Greene <russellgreene8@gmail.com>
2017-04-14 17:36:19 -0600

    Remove tests from travis

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,30 +4,13 @@ set -xe
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 
-	if [ -n "$TEST_COV" ]; then
-
-		cmake . -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-			-DCMAKE_CXX_COMPILER=$CXX_COMPILER \
-			-DCMAKE_C_COMPILER=$C_COMPILER \
-			-GNinja -DCMAKE_CXX_FLAGS='--coverage' \
-			-DLLVM_CONFIG="/usr/lib/llvm-${LLVM_VERSION}/bin/llvm-config"
-	else
-
-		cmake . -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-			-DCMAKE_CXX_COMPILER=$CXX_COMPILER \
-			-DCMAKE_C_COMPILER=$C_COMPILER \
-			-GNinja \
-			-DLLVM_CONFIG="/usr/lib/llvm-${LLVM_VERSION}/bin/llvm-config"
-
-	fi
+	cmake . -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+		-DCMAKE_CXX_COMPILER=$CXX_COMPILER \
+		-DCMAKE_C_COMPILER=$C_COMPILER \
+		-GNinja -DCMAKE_CXX_FLAGS='--coverage' \
+		-DLLVM_CONFIG="/usr/lib/llvm-${LLVM_VERSION}/bin/llvm-config"
 
 	ninja
-	CTEST_OUTPUT_ON_FAILURE=1 ninja test
-
-	if [ -n "$TEST_COV" ]; then
-		pip install --user cpp-coveralls
-		coveralls --exclude /usr --exclude third_party --exclude chigraphgui --exclude test --exclude build --include libchigraph --include libchigraphdebugger --include chi --exclude libchigraph/include/chi/json.hpp
-	fi
 
 else
 
@@ -37,6 +20,5 @@ else
 		-DLLVM_CONFIG=/usr/local/opt/llvm/bin/llvm-config \
 		-GNinja -DCG_BUILD_DEBUGGER=OFF
 	ninja
-	# ninja test
 
 fi
