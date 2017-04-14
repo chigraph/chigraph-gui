b98b9a3c6a0f91d7e8b7514f1e56c5e2f151fe77
Aun-Ali Zaidi <admin@kodeit.net>
2017-04-14 04:29:04 -0500

    travis/install.sh: Workaround KCoreAddons file conflicts.
    Signed-off-by: Aun-Ali Zaidi <admin@kodeit.net>

--- .travis/install.sh
+++ .travis/install.sh
@@ -19,15 +19,19 @@ else
 
 	brew tap chigraph/kf5
 	brew install chigraph/kf5/kf5-extra-cmake-modules
+
+	# kcoreaddons workaround
+	brew install chigraph/kf5/kf5-kcoreaddons
+	brew link --force --overwrite chigraph/kf5/kf5-kcoreaddons
+
 	brew install chigraph/kf5/kf5-karchive		chigraph/kf5/kf5-ktexteditor		chigraph/kf5/kf5-kjobwidgets	\
 		     chigraph/kf5/kf5-kdbusaddons	chigraph/kf5/kf5-kio			chigraph/kf5/kf5-kcrash		\
 		     chigraph/kf5/kf5-sonnet		chigraph/kf5/kf5-syntax-highlighting	chigraph/kf5/kf5-kparts		\
 		     chigraph/kf5/kf5-kguiaddons	chigraph/kf5/kf5-kitemviews		chigraph/kf5/kf5-kconfig	\
 		     chigraph/kf5/kf5-kconfigwidgets	chigraph/kf5/kf5-kauth			chigraph/kf5/kf5-kcodecs	\
 		     chigraph/kf5/kf5-kcompletion	chigraph/kf5/kf5-kglobalaccel		chigraph/kf5/kf5-kservice	\
-		     chigraph/kf5/kf5-kwindowsystem	chigraph/kf5/kf5-kcoreaddons		chigraph/kf5/kf5-ki18n		\
-		     chigraph/kf5/kf5-kxmlgui		chigraph/kf5/kf5-kwidgetsaddons		chigraph/kf5/kf5-ktextwidgets	\
-		     chigraph/kf5/kf5-kiconthemes
+		     chigraph/kf5/kf5-kwindowsystem	chigraph/kf5/kf5-ki18n			chigraph/kf5/kf5-kxmlgui	\
+		     chigraph/kf5/kf5-kwidgetsaddons	chigraph/kf5/kf5-ktextwidgets		chigraph/kf5/kf5-kiconthemes
 
 	bash ./setup.sh
 
